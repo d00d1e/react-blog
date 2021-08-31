cb4a0cecccf91643195676fdd72f2cfad6cfb6e6
d00d1e <lee.julianay@gmail.com>
2021-08-31 15:01:28 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import iconImg from "../../img/unnamed.jpeg";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navLeft">
+        <i className="navIcon fab fa-facebook-square"></i>
+        <i className="navIcon fab fa-twitter-square"></i>
+        <i className="navIcon fab fa-instagram-square"></i>
+        <i className="navIcon fab fa-pinterest-square"></i>
+      </div>
+      <div className="navCenter">
+        <ul className="navList">
+          <li className="navListItem">Home</li>
+          <li className="navListItem">About</li>
+          <li className="navListItem">Contact</li>
+          <li className="navListItem">Write</li>
+          <li className="navListItem">Logout</li>
+        </ul>
+      </div>
+      <div className="navRight">
+        <img className="navImg" src={iconImg} alt="Profile Img" />
+        <i className="searchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
