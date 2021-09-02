72c86189c8a42f97d99b96127f9070366aea3bff
d00d1e <lee.julianay@gmail.com>
2021-09-01 22:43:39 -0700

    add settings component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Write from "./pages/Write/Write";
 import Home from "./pages/Home/Home";
 import Postpage from "./pages/PostPage/PostPage";
+import Settings from "./pages/Settings/Settings";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       <Navbar />
       {/* <Home /> */}
       {/* <Postpage /> */}
-      <Write />
+      {/* <Write /> */}
+      <Settings />
     </>
   );
 }
---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Sidebar from "../../components/Sidebar/Sidebar";
+import "./Settings.css";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Image</label>
+          <div className="settingsProfileImg">
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsProfileImgIcon far fa-user-circle"></i>
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsProfileImgInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Juliana" name="name" />
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="lee.julianay@gmail.com"
+            name="email"
+          />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+
+      <Sidebar />
+    </div>
+  );
+}
