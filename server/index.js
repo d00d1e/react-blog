130fd14d251be557d0c458371ba137a7fb58443b
d00d1e <lee.julianay@gmail.com>
2021-09-02 20:34:50 -0700

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
