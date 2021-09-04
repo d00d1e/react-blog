c54d6ba9688c9b5fb3472feb5bf0791fa66b0753
d00d1e <lee.julianay@gmail.com>
2021-09-04 15:40:30 -0700

    upload files with multer

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import multer from "multer";
 
 import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/userRouter.js";
@@ -24,6 +25,21 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
+
+//STORAGE
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json({ message: "File has been uploaded" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
