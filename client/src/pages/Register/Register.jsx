4b849f67130a269401ec6efd17c8eea5d6849d18
d00d1e <lee.julianay@gmail.com>
2021-09-07 01:01:59 -0700

    client side user registration

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import "./Register.css";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError(false);
+
+    try {
+      const { data } = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      data && window.location.replace("/login");
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input className="registerInput" type="text" />
+        <input
+          className="registerInput"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
         <label>Email</label>
-        <input className="registerInput" type="email" />
+        <input
+          className="registerInput"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>Password</label>
-        <input className="registerInput" type="password" />
+        <input
+          className="registerInput"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button className="registerButton">Register</button>
+        <button className="registerButton" type="submit">
+          Register
+        </button>
       </form>
 
       <button className="registerLoginButton">
@@ -25,6 +64,8 @@ export default function Register() {
           Login
         </Link>
       </button>
+
+      {error && <span className="registerError">Something went wrong!</span>}
     </div>
   );
 }
